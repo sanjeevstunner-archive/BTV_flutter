397ea35c7d651ef1178b32e9473adac2f408b629
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-17 09:51:55 +0530

    app: pages: home screen: Lot of changes to track
    * Switch to stateful widget
    * Implemented web player
    * Add SVG assets for socials
    
    * pub: Update dependencies

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:better_player/better_player.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late BetterPlayerController _betterPlayerController;
+  GlobalKey _betterPlayerKey = GlobalKey();
+
+  @override
+  void initState() {
+    BetterPlayerConfiguration betterPlayerConfiguration =
+        BetterPlayerConfiguration(
+      aspectRatio: 16 / 9,
+      fit: BoxFit.contain,
+    );
+    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      Constants.sourceVideoUrl,
+    );
+    _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
+    _betterPlayerController.setupDataSource(dataSource);
+    _betterPlayerController.setBetterPlayerGlobalKey(_betterPlayerKey);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.background,
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: BetterPlayer(
+              controller: _betterPlayerController,
+              key: _betterPlayerKey,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text("Switch to mini player"),
+            onPressed: () {
+              _betterPlayerController.enablePictureInPicture(_betterPlayerKey);
+            },
+          ),
+          Row(
+            children: [
+              SvgPicture.asset(Assets.whatsapp),
+              SvgPicture.asset(Assets.facebook),
+              SvgPicture.asset(Assets.instagram),
+              SvgPicture.asset(Assets.twitter),
+              SvgPicture.asset(Assets.sharechat),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
