823fb6525b1681bd3e2d027508fed3644187ca83
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-17 09:38:01 +0530

    app: Import assets and define constants

---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const String sourceVideoUrl =
+      "http://hdserver.7starcloud.com:1935/atntv/live/playlist.m3u8";
+}
+
+class Assets {
+  static const String _images = 'assets/images/';
+
+  static const String facebook = '${_images}Facebook.svg';
+  static const String instagram = '${_images}Instagram.svg';
+  static const String sharechat = '${_images}Sharechat.svg';
+  static const String twitter = '${_images}Twitter.svg';
+  static const String whatsapp = '${_images}Whatsapp.svg';
+}
+
+class AppColors {
+  static const Color background = Colors.black;
+}
